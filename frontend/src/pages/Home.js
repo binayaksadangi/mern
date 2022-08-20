6d5906194e8cf677e31f6450523cb8f26a043843
binayaksadangi <youbinayak@gmail.com>
2022-08-21 01:53:36 +0530

    get-all-data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+//components
+import WorkoutDetails from "../components/WorkoutDetails"
 
-function Home () {
+const Home = () => {
+    const [workouts,setWorkouts] = useState(null);
+    useEffect(()=>{
+        const fetchWorkouts = async () => {
+            const response = await fetch('api/workouts')
+            const json = await response.json();
+
+            if(response.ok) {
+                setWorkouts(json);
+            }
+        }
+        fetchWorkouts()
+    }, [])
     return(
         <div className="home">
-            <h1>All Workouts</h1>
+             <div className="workouts">
+                {workouts && workouts.map((workout)=>(
+                  <WorkoutDetails key={workout._id} workout={workout}/>
+                ))}
+             </div>
+            
         </div>
     )
 }
